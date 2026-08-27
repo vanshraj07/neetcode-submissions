da667fbc1fb92deb987733257ab8ca832aebac00
Vansh Raj Singh <161923096+vanshraj07@users.noreply.github.com>
2026-08-27 20:11:22 +0530

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans;
+        int maxi = INT_MIN;
+        for (int i = 0; i < k; i++) {
+            maxi = max(maxi, nums[i]);
+        }
+        ans.push_back(maxi);
+        int l = 1;
+        int r = k;
+        while (r < n) {
+            if (nums[l - 1] != maxi) {
+                maxi = max(maxi, nums[r]);
+            }
+            else {
+                maxi = nums[l];
+                for (int i = l; i <= r; i++) {
+                    maxi = max(maxi, nums[i]);
+                }
+            }
+            ans.push_back(maxi);
+            l++;
+            r++;
+        }
+        return ans;
+    }
+};
