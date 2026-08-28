457726025a5e88421e6d4331c7ee555767f2cf52
Vansh Raj Singh <161923096+vanshraj07@users.noreply.github.com>
2026-08-28 16:59:16 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,28 @@
+class TimeMap {
+    unordered_map<string,vector<pair<int,string>>>mpp;
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mpp[key].push_back({timestamp,value});
+    }
+    
+    string get(string key, int timestamp) {
+        int low=0;
+        int high=mpp[key].size()-1;
+        string ans="";
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(mpp[key][mid].first<=timestamp){
+                ans=mpp[key][mid].second;
+                low=mid+1;
+            }
+            else {
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
